7b0738361c1257e0c0f0098b02ab255e6d78365b
hmpb-odoo <hmpb@odoo.com>
2022-06-13 16:21:10 +0000

    [ADD] Basic volunteers module

---
+++ cooperative_volunteers/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Cooperative Volunteers',
+    
+    'sumary': 'App to manage a cooperative shop',
+    
+    'description': """
+        Cooperative Volunteers app to manage a coperative shop:
+        -Volunteers
+        -Shop
+    """,
+    
+    'author': 'hmpb',
+    
+    'category': 'Management',
+    
+    'version': '0.1',
+    
+    'depends': ['base'],
+    
+    'data': [
+        
+    ],
+    
+   'demo': [
+       
+   ],
+    
+}
